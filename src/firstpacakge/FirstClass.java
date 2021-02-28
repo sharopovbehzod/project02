2f49827d0e8cb32a7ceb39c73698ea990590d092
sharopovbehzod <79700041+sharopovbehzod@users.noreply.github.com>
2021-02-28 08:22:51 -0500

    second commit from remote master branch

--- src/firstpacakge/FirstClass.java
+++ src/firstpacakge/FirstClass.java
@@ -8,7 +8,7 @@ public class FirstClass {
 		System.out.println("second commit from local master");
                 System.out.println("third commit from local master");
 		System.out.println("first commit  from remote master branch");
-		System.out.println("second commit  from remote master branch");
+		System.out.println("second commit from remote master branch");
         
 		
 		
