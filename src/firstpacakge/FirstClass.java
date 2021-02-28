2ef6ce2f866c209daa8c457c883ead2e4c424674
sharopovbehzod <sharopovbehzod@gmail.com>
2021-02-28 08:03:33 -0500

    second commit from local ,aster

--- src/firstpacakge/FirstClass.java
+++ src/firstpacakge/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("first commit from from local branch");
-		
+		System.out.println("second commit from local master");
 
 	}
 
